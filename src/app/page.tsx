37821676c3428126da05a4d40f3c20ce5d4f8428
JavierCanoy <javier@mabolotech.com>
2023-07-30 11:13:17 +0800

    practs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,15 +16,24 @@ import BounceLoader from "react-spinners/BounceLoader";
 import PacmanLoader from "react-spinners/PacmanLoader";
 import { useErrorBoundary } from "react-error-boundary";
 import { ErrorBoundary } from "react-error-boundary";
-
-PacmanLoader;
+import GetPic from "./pic/page";
 
 export default function Main() {
   return (
     <div className="main  w-full">
-      <NavBar />
+      {/* <NavBar /> */}
       <div className="  ">
         <div className=" mt-10 grid grid-cols-2 justify-items-center  ">
+          {/* <SkeletonTheme
+            // properties
+            baseColor="#0C6BA3"
+            highlightColor="#012C40"
+            width={300}
+          >
+            <Suspense fallback={<Skeleton className="   my-2  " count={10} />}>
+              <GetPic />
+            </Suspense>
+          </SkeletonTheme> */}
           <div className=" image-using-skeleton-and-snipper">
             <h1 className=" my-10 font-bold uppercase pt-20">
               display image using skeleton !

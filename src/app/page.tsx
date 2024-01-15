680e6e0ecd6fd950461048be1267d8f762d5fc30
JavierCanoy <javier@mabolotech.com>
2024-01-15 20:28:04 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 "use client";
-import NavBar from "../../components/NavBar";
-import Home from "../../components/Home";
-import About from "../../components/About";
-import Contact from "../../components/Contact";
-import Footer from "../../components/Footer";
-import Projects from "../../components/Projects";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Projects from "./components/Projects";
 import React from "react";
 
 export default function Page() {
   return (
-    <div>
+    <div className="main">
       <NavBar />
       <Home />
       <About />
